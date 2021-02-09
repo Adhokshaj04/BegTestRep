7f1653b5d4ee87e21ce5841ed50ab2fad6acf4af
Adhokshaj04 <51985285+Adhokshaj04@users.noreply.github.com>
2021-02-09 20:37:21 +0400

    Add files via upload
    New File Uploaded

---
+++ prog1.py
@@ -0,0 +1,19 @@
+Ed="In second year"
+Salary="with a salary of 200,000"
+print(Ed,Salary)
+
+abc="We are learning python"
+print(abc[16:])
+mystr="hello world"
+print(mystr[1])
+print(mystr[5])
+print(mystr[0:5])
+
+print(len("adhok"))
+
+name='my\'name\'is"abc"'
+print(name)
+
+name='abc'
+age=20
+print('My name is '+name+' and age is '+str(age))
